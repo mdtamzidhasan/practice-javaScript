64e7d194ada58cc7a14f46e52135d8c626df7f62
mdtamzidhasan <tamzidhasan2020@gmail.com>
2024-10-09 22:36:53 +0600

    start function

---
+++ function1.js
@@ -0,0 +1,9 @@
+function fanOffKor()
+{
+    console.log('boisa theke uitha dara');
+    console.log('walk towards the switch');
+    console.log('click the switch to off of the fan');
+
+}
+
+fanOffKor();
