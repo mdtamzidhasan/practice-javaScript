836ed89c660285fcc06d1ba96f81ba1d0fe137bd
mdtamzidhasan <tamzidhasan2020@gmail.com>
2024-10-10 12:29:05 +0600

    practice function

---
+++ FunRet.js
@@ -0,0 +1,14 @@
+function tentimes(number)
+{
+    const result=number*10;
+    return result;
+}
+
+function cutHalf(number)
+{
+    const half=number/2;
+    return half;
+}
+
+const output = tentimes(5);
+console.log('Output: ',output);
---
+++ funWithParam.js
@@ -0,0 +1,22 @@
+function square(number)
+{
+    console.log('value of the number parameter: ',number);
+    const borgo = number * number;
+    console.log('square of the given number is: ',borgo);
+}
+square(4);
+console.log('-------------');
+square(5);
+console.log('-------------');
+square(6);
+console.log('-------------');
+
+
+
+function add(num1, num2)
+{
+    const sum = num1 + num2;
+    console.log(sum);
+}
+const num1=3;
+add(num1,5);
